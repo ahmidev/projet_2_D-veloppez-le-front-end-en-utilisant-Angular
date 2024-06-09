eeaa13c2678af134012f858dfde4894528bd81b0
ahmidev <ahmid.aitouali@gmail.com>
2024-06-09 20:19:15 +0200

    feat: create interface, type request data, and handle errors

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<Olympic[]>([]);
+  private isLoading$ = new BehaviorSubject<boolean>(false);
+  private errorMessage$ = new BehaviorSubject<string | null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
-      tap((value) => this.olympics$.next(value)),
-      catchError((error, caught) => {
-        // TODO: improve error handling
+    this.isLoading$.next(true); 
+    this.errorMessage$.next(null); 
+
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
+      tap((value) => {
+        this.olympics$.next(value); 
+        this.isLoading$.next(false); 
+      }),
+      catchError((error) => {
         console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
-        return caught;
+        this.olympics$.next([]);
+        this.isLoading$.next(false); 
+        this.errorMessage$.next('Échec du chargement des données olympiques. Veuillez réessayer plus tard.');
+        
+        
+        return of([]);
       })
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<Olympic[]> {
     return this.olympics$.asObservable();
+  }
+
+  getIsLoading$(): Observable<boolean> {
+    return this.isLoading$.asObservable();
+  }
+
+  getErrorMessage$(): Observable<string | null> {
+    return this.errorMessage$.asObservable();
   }
 }

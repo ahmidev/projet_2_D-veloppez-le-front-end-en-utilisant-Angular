487d244f049e8bce640fd9d94ca2951d2f7c7df6
ahmidev <ahmid.aitouali@gmail.com>
2024-06-10 09:12:57 +0200

    feat:Add missing files that were not previously committed

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Olympic } from '../models/Olympic';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,7 @@ export class OlympicService {
   private isLoading$ = new BehaviorSubject<boolean>(false);
   private errorMessage$ = new BehaviorSubject<string | null>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,  private router: Router) {}
 
   loadInitialData() {
     this.isLoading$.next(true); 
@@ -28,7 +29,12 @@ export class OlympicService {
         console.error(error);
         this.olympics$.next([]);
         this.isLoading$.next(false); 
-        this.errorMessage$.next('Échec du chargement des données olympiques. Veuillez réessayer plus tard.');
+        if (error.status === 404) {
+          this.errorMessage$.next('URL non trouvée. Redirection vers la page Not Found.');
+          this.router.navigate(['/not-found']);
+        } else {
+          this.errorMessage$.next('Échec du chargement des données olympiques. Veuillez réessayer plus tard.');
+        }
         
         
         return of([]);
